c9dbe03b2a54250cab9d6c191325a71bbaa35d37
snoorghorbani <snoorghorbani@gmail.com>
2016-07-22 17:28:31 +0430

    1.3.4

--- generators/directive/create_file.js
+++ generators/directive/create_file.js
@@ -8,7 +8,12 @@ var create_file = function () {
         var destination_path = function () {
             return ('./' + generator.directory + '/' + generator.moduleName.underscored + '/directives/' + getName());
         }
+
         generator.template(templatesFolder + '_directive.js', destination_path() + '.directive.js');
+        if (generator.haveView) {
+            generator.templateUrl = destination_path() + '.directive.html';
+            generator.template(templatesFolder + '_directive.html', generator.templateUrl);
+        }
     }
 };
 
--- templates/_directive.js
+++ templates/_directive.js
@@ -5,7 +5,8 @@ angular
                 restrict: 'EA',
                 priority: 1500,
                 transclude: false,
-                template: '',
+                <% if (!haveView) { %>template: '<div><%= name.titleize %><div>', <% } %>
+                <% if (haveView) { %>templateUrl: <%= templateUrl %>,<% } %>
                 require: [],
                 scope: false,
                 controller: ['$scope', '$element', '$attrs', '$compile', 'share_module_config', "_", function ($scope, element, attrs, $compile, share_module_config, _) {
